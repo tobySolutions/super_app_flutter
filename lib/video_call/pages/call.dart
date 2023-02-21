fc4b74283c94f7cf7152f9045d18695bfc729a53
SamuelBasseyJohn <88030818+SamuelBasseyJohn@users.noreply.github.com>
2023-02-21 08:32:40 +0100

    Progress on video call

--- lib/video_call/pages/call.dart
+++ lib/video_call/pages/call.dart
@@ -1,3 +1,4 @@
+import 'package:blue_app/video_call/utils/setting.dart';
 import 'package:flutter/material.dart';
 import 'package:agora_rtc_engine/agora_rtc_engine.dart';
 
@@ -11,6 +12,37 @@ class CallPage extends StatefulWidget {
 }
 
 class _CallPageState extends State<CallPage> {
+  final _users = <int>[];
+  final _infoStrings = <String>[];
+  bool mute = false;
+  RtcEngine? _engine;
+
+  @override
+  void initState() async{
+    super.initState();
+    initialize();
+  }
+
+  @override
+  void dispose() {
+    _users.clear();
+    _engine!.leaveChannel();
+    
+
+    super.dispose();
+  }
+
+  Future<void> initialize() async {
+    if (appId.isEmpty) {
+      setState(() {
+        _infoStrings.add("App ID is missing, Please provide App ID");
+        _infoStrings.add("Agora Engine is not starting");
+      });
+      return;
+    }
+    _engine!.cr\
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/video_call/pages/index.dart
+++ lib/video_call/pages/index.dart
@@ -1,4 +1,15 @@
+import 'package:agora_rtc_engine/agora_rtc_engine.dart';
+import 'package:blue_app/video_call/pages/call.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import 'dart:async';
+
+import 'package:permission_handler/permission_handler.dart';
+
+// import 'package:agora_rtc_engine/agora_rtc_engine.dart';
+
+// import 'package:permission_handler/permission_handler.dart';
 
 class IndexPage extends StatefulWidget {
   const IndexPage({super.key});
@@ -8,6 +19,16 @@ class IndexPage extends StatefulWidget {
 }
 
 class _IndexPageState extends State<IndexPage> {
+  final _channelController = TextEditingController();
+  bool _validateError = false;
+  ClientRoleType _role = ClientRoleType.clientRoleBroadcaster;
+
+  @override
+  void dispose() {
+    super.dispose();
+    _channelController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,6 +36,76 @@ class _IndexPageState extends State<IndexPage> {
         title: const Text("Agora"),
         centerTitle: true,
       ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: <Widget>[
+              TextField(
+                controller: _channelController,
+                decoration: InputDecoration(
+                    errorText:
+                        _validateError ? "Channel name is mandatory" : null,
+                    border: const UnderlineInputBorder()),
+              ),
+              RadioListTile(
+                title: const Text('Broadcaster'),
+                value: ClientRoleType.clientRoleBroadcaster,
+                groupValue: _role,
+                onChanged: (value) {
+                  setState(() {
+                    if (value is ClientRoleType) {
+                      _role = value;
+                    }
+                  });
+                },
+              ),
+              RadioListTile(
+                title: const Text('Audience'),
+                value: ClientRoleType.clientRoleAudience,
+                groupValue: _role,
+                onChanged: (value) {
+                  setState(() {
+                    if (value is ClientRoleType) {
+                      _role = value;
+                    }
+                  });
+                },
+              ),
+              ElevatedButton(
+                onPressed: onJoin,
+                style: ElevatedButton.styleFrom(
+                    minimumSize: const Size(double.infinity, 40)),
+                child: const Text("Join"),
+              )
+            ],
+          ),
+        ),
+      ),
     );
+  }
+
+  Future<void> onJoin() async {
+    setState(() {
+      _channelController.text.isEmpty
+          ? _validateError = true
+          : _validateError = false;
+    });
+    if (_channelController.text.isNotEmpty) {
+      await _handleCameraAndMic(Permission.camera);
+      await _handleCameraAndMic(Permission.microphone);
+      await Navigator.push(
+          context,
+          CupertinoPageRoute(
+              builder: (context) => CallPage(
+                    channelName: _channelController.text,
+                    clientRole: _role,
+                  )));
+    }
+  }
+
+  Future<void> _handleCameraAndMic(Permission permission) async {
+    final status = await permission.request();
+    debugPrint(status.toString());
   }
 }
